13a5bfca527b842dddaa2448c77f1166e90cd87c
Zeffuro <Jeffroiscool@gmail.com>
2025-08-09 18:31:45 +0200

    Initial commit

---
+++ NativeMeters/Plugin.cs
@@ -0,0 +1,27 @@
+using Dalamud.Plugin;
+using Dalamud.Game.Command;
+
+namespace NativeMeters;
+
+public class Plugin : IDalamudPlugin
+{
+    public const string CommandName = "/nativemeters";
+
+    public Plugin(IDalamudPluginInterface pluginInterface)
+    {
+        pluginInterface.Create<Services>();
+
+        Services.CommandManager.AddHandler(CommandName, new CommandInfo(this.OnCommand)
+        {
+            HelpMessage = ""
+        });
+    }
+
+    public void Dispose()
+    {
+        Services.CommandManager.RemoveHandler(CommandName);
+    }
+
+    private void OnCommand(string command, string args)
+    {
+    }
---
+++ NativeMeters/Services.cs
@@ -0,0 +1,11 @@
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+
+namespace NativeMeters;
+
+public class Services
+{
+    [PluginService] public static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
+    [PluginService] public static ICommandManager CommandManager { get; private set; } = null!;
+}
