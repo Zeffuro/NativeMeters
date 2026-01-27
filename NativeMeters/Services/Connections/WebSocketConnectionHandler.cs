7226eec96e019b83e599240ba3b49b2b462c8d66
Zeffuro <Jeffroiscool@gmail.com>
2026-01-27 11:16:26 +0100

    Fix leaking handlers

--- NativeMeters/Services/Connections/WebSocketConnectionHandler.cs
+++ NativeMeters/Services/Connections/WebSocketConnectionHandler.cs
@@ -9,23 +9,37 @@ public class WebSocketConnectionHandler(WebSocketClient client) : IConnectionHan
     public event Action? OnDisconnected;
     public event Action<string>? OnMessageReceived;
 
+    private Action? connectedHandler;
+    private Action? disconnectedHandler;
+    private Action<string>? messageHandler;
+
     public bool IsConnected => client.IsConnected;
 
     public void Start()
     {
-        client.OnConnected += () => OnConnected?.Invoke();
-        client.OnDisconnected += () => OnDisconnected?.Invoke();
-        client.OnMessageReceived += msg => OnMessageReceived?.Invoke(msg);
+        connectedHandler = () => OnConnected?.Invoke();
+        disconnectedHandler = () => OnDisconnected?.Invoke();
+        messageHandler = msg => OnMessageReceived?.Invoke(msg);
+
+        client.OnConnected += connectedHandler;
+        client.OnDisconnected += disconnectedHandler;
+        client.OnMessageReceived += messageHandler;
+
         _ = client.StartAsync(new Uri(System.Config.ConnectionSettings.WebSocketUrl));
     }
 
     public void Stop()
     {
-        client.OnConnected -= () => OnConnected?.Invoke();
-        client.OnDisconnected -= () => OnDisconnected?.Invoke();
-        client.OnMessageReceived -= msg => OnMessageReceived?.Invoke(msg);
+        if (connectedHandler != null) client.OnConnected -= connectedHandler;
+        if (disconnectedHandler != null) client.OnDisconnected -= disconnectedHandler;
+        if (messageHandler != null) client.OnMessageReceived -= messageHandler;
+
+        connectedHandler = null;
+        disconnectedHandler = null;
+        messageHandler = null;
+
         _ = client.StopAsync();
     }
 
-    public void Dispose() => client.Dispose();
+    public void Dispose() => Stop();
 }
